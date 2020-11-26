19918e7da982356a00d6449275e49d4ce79325f4
Ezequiel E. Vera <37745672+ezeveritta@users.noreply.github.com>
2020-11-26 12:05:47 -0300

    Trabajo de Investigación

---
+++ app/views/actions/comprimir.php
@@ -0,0 +1,35 @@
+<?php
+
+// Obtengo información de POST
+if (count($_POST) < 1) {
+    header("Location: ../contenido.php?aviso=Error:+no+se+proporcionó+un+archivo+para+comprimir");
+    die();
+}
+$datos = $_POST;
+
+// Defino información del archivo comprimido
+$objZip       = new ZipArchive();
+$rutaDestino  = '../../../public/files/';
+$rutaArchivos = '../../../public/files/';
+$objDatetime  = new DateTime();
+$nombreZip    = $objDatetime->format('H_m_s') . '.zip';
+
+// Creo el archivo zip
+if ($objZip->open($rutaDestino.$nombreZip, ZIPARCHIVE::CREATE) === true) {
+
+    // Añadimos los archivos
+    foreach ($datos as $nombreArchivo) {
+        $objZip->addFile($rutaArchivos . $nombreArchivo, $nombreArchivo); 
+    }
+
+    // Guardamos
+    $objZip->close();
+
+    // Redireccionamos a "contenido.php"
+    header("Location: ../contenido.php?exito=Archivo+comprimido+con+exito!");
+    die();
+
+} else {
+    header("Location: ../contenido.php?error=Error+al+comprimir");
+    die();
+}
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Investigación - Archivos Comprimidos</title>
+</head>
+<body>
+    <a href="app/views/contenido.php">Página para comprimir</a>
+    <br>
+    <a href="app/views/phpInfo.php">PhpInfo</a>
+</body>
+</html>
